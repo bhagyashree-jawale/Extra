928c651ebe321817c501797dcf04f0f29826ee33
bhagyashree-jawale <bmjj1612@gmail.com>
2017-04-14 16:42:41 +0530

    Hello world running with @RequestParam and @PathVariable

--- ShopForntEnd/src/main/java/com/niit/controller/PageController.java
+++ ShopForntEnd/src/main/java/com/niit/controller/PageController.java
@@ -1,14 +1,35 @@
 package com.niit.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PageController {
-	@RequestMapping(value = "/")
+	@RequestMapping(value = {"/","/home","/index"})
 	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting","welcome in e-shopping");
+		return mv;
+	}
+	
+//	@RequestMapping(value = "/test")
+//	public ModelAndView test(@RequestParam(value="greeting",required=false) String greeting) {
+//		if(greeting==null)
+//			greeting="Hello world";
+//		ModelAndView mv = new ModelAndView("page");
+//		mv.addObject("greeting",greeting);
+//		return mv;
+//	}
+	
+	@RequestMapping(value = "/test/{greeting}")
+	public ModelAndView test(@PathVariable("greeting") String greeting) {
+		if(greeting==null)
+			greeting="Hello world";
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting",greeting);
 		return mv;
 	}
 }
